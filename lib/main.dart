91948c7159881d4e9e0663426843bc1d80b9eb55
khurbnaovrakhmiddin <khurbanovrakhmiddin@gmail.com>
2023-06-22 22:32:51 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:satisfaction/send_bot_message.dart';
 
+import 'history_page.dart';
 import 'netservice.dart';
 
 void main() async {
@@ -33,6 +34,130 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _name;
+  late TextEditingController _type;
+  late TextEditingController _summa;
+
+  @override
+  void initState() {
+    super.initState();
+    _name = TextEditingController();
+    _type = TextEditingController();
+    _summa = TextEditingController();
+  }
+
+  String priceParser(var a) {
+
+
+
+    String result = a.toString();
+    String res = '';
+
+
+    switch (result.length) {
+      case 4:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 1) {
+              res += ' ';
+            }
+
+            res += result[i];
+          }
+          break;
+        }
+      case 5:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 2) {
+              res += ' ';
+            }
+
+            res += result[i];
+          }
+          break;
+        }
+      case 6:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 3) {
+              res += ' ';
+            }
+
+            res += result[i];
+          }
+          break;
+        }
+      case 7:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 1) {
+              res += ' ';
+            }
+            if (i == 4) {
+              res += ' ';
+            }
+
+            res += result[i];
+          }
+          break;
+        }
+      case 8:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 2) {
+              res += ' ';
+            }
+            if (i == 5) {
+              res += ' ';
+            }
+            if (i == 8) {
+              res += ' ';
+            }
+
+            res += result[i];
+          }
+          break;
+        }
+      case 9:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 3) {
+              res += ' ';
+            }
+            if (i == 6) {
+              res += ' ';
+            }
+            if (i == 9) {
+              res += ' ';
+            }
+            res += result[i];
+          }
+          break;
+        }
+      case 10:
+        {
+          for (int i = 0; i < result.length; i++) {
+            if (i == 1) {
+              res += ' ';
+            }
+            if (i == 4) {
+              res += ' ';
+            }
+            if (i == 7) {
+              res += ' ';
+            }
+            if (i == 10) {
+              res += ' ';
+            }
+
+            res += result[i];
+          }
+          break;
+        }
+    }
+    return res;
+  }
   var decoration = InputDecoration(
       filled: true,
       fillColor: Colors.blue.shade100,
@@ -40,6 +165,48 @@ class _MyHomePageState extends State<MyHomePage> {
         borderRadius: BorderRadius.circular(10),
         borderSide: BorderSide.none,
       ));
+
+  bool full = true;
+  bool loading = false;
+  String message = '';
+
+  void accept() async {
+    String name = _name.text.toString();
+    String type = _type.text.toString();
+    String summa = _summa.text.toString();
+
+    print(name);
+    print(type);
+    print(summa);
+    print(full);
+
+
+    if (name.isEmpty || type.isEmpty || summa.isEmpty) {
+      full = false;
+      setState(() {});
+      return;
+    }
+    loading = true;
+
+      full = true;
+      setState(() {});
+
+    try {
+
+
+      await NetService.SENDTELEGRAMBOT(name: name, type: type, sum: priceParser(summa));
+      loading = false;
+setState(() {
+
+});
+    } catch (e) {
+      message = e.toString();
+
+      full = false;
+      loading = false;
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,20 +220,21 @@ class _MyHomePageState extends State<MyHomePage> {
             Center(
               child: Text(
                 "Расходы ${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}",
-                style: TextStyle(fontSize: 25, color: Colors.black),
+                style: const TextStyle(fontSize: 25, color: Colors.black),
               ),
             ),
             ...text("Имя товара"),
-            TextField(decoration: decoration.copyWith(hintText: "Имя товара")),
+            TextField(controller: _name, decoration: decoration.copyWith(hintText: "Имя товара")),
             ...text("Тип расхода"),
-            TextField(decoration: decoration.copyWith(hintText: "Тип расхода")),
+            TextField(controller: _type, decoration: decoration.copyWith(hintText: "Тип расхода")),
             ...text("Общая сумма"),
             TextField(
+                controller: _summa,
                 keyboardType: TextInputType.number,
                 decoration: decoration.copyWith(
                   hintText: "Общая сумма",
                 )),
-            SizedBox(
+           const SizedBox(
               height: 50,
             ),
             ElevatedButton(
@@ -80,8 +248,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(12),
                     ))),
-                onPressed: () {},
-                child: Text("Сохранить"))
+                onPressed: accept,
+                child:loading?const CircularProgressIndicator():const Text("Сохранить"))
           ],
         ),
       ),
